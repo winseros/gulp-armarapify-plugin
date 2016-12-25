4af93ef9c27dfd1fb5970085fe067260a64d49b9
Nikita Kobzev <win.accuracy@gmail.com>
2016-12-25 18:35:51 +0300

    Added code coverage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,12 +5,17 @@ import sourcemaps from 'gulp-sourcemaps';
 import jasmine from 'gulp-jasmine';
 import consoleReporter from 'jasmine-console-reporter';
 import watch from 'gulp-watch';
+import istanbul from 'gulp-istanbul';
+import remapIstanbul from 'remap-istanbul/lib/gulpRemapIstanbul';
 import del from 'del';
+import path from 'path';
 
 const dist = './dist';
 const sourceFiles = './src/**/*.ts';
 const typingFiles = './typings/**/index.d.ts';
-const testFiles = './dist/**/*.spec.js';
+const testSpecs = './dist/**/*.spec.js';
+const testSources = ['./dist/**/*.js', `!${testSpecs}`];
+const coverageDir = './.coverage'
 
 gulp.task('clean', () => {
     return del([`${dist}/*`]);
@@ -38,12 +43,34 @@ gulp.task('assemble', ['clean', 'tslint'], () => {
         .pipe(gulp.dest(dist));
 });
 
-gulp.task('test', ['assemble'], () => {
-    return gulp.src(testFiles)
+gulp.task('test:run', ['assemble'], () => {
+    return gulp.src(testSpecs)
         .pipe(jasmine({
             verbose: true,
             reporter: new consoleReporter()
         }));
+});
+
+gulp.task('cover:instrument', () => {
+    return gulp.src(testSources)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('cover:run', ['cover:instrument'], () => {
+    const task = gulp.src(testSpecs)
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports({
+            dir: dist,
+            reporters: ['json']
+        }));
+
+    task.on('end', () => gulp.src(path.join(dist, 'coverage-final.json'))
+        .pipe(remapIstanbul({
+            reports: { html: coverageDir }
+        })));
+
+    return task;
 });
 
 gulp.task('watch', ['assemble'], () => {
